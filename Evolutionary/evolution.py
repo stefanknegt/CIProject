6a5d354624c736d9ed0404a56bd423f101f026c7
Unknown <tom@vd.nl>
2017-11-16 16:50:55 +0100

    Moved some files

--- Evolutionary/evolution.py
+++ Evolutionary/evolution.py
@@ -1,15 +1,19 @@
-population = 100
-fitnesses = np.zeros(100)
-terminate = False
-while terminate == False:
+from pytocl.main import main
+from my_driver_evo import MyDriver
+import numpy as np
+from evolution_nn import
+
+def calculate_fitness():
+    population = 100
+    except:
+        laptimes = np.load('laptimes.npy')
+    as:
+        print("Making a new laptime matrix")
+        laptimes = np.zeros(population)
+        np.save('laptimes.npy', laptimes)
     for i in range(0,population):
-        name = "evolutionary_"+str(i)
-        model = load_model()
-        
-    fitnesses[i] = test(model)
-  death_idx = fitness.argsort()[:birth]
-  parent_pool = fitness.argsort()[parents:]
-  for i in death_idx:
-    parent = np.random.choice(parents, 1, replace=False)
-    parent_model = load_model(parent)
-    model = mutate_model(parent_model)
+        main(MyDriver(i))
+
+def generate_children():
+    laptimes = np.load('laptimes.npy')
+    fitness = 
